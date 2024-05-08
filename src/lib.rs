4a3c3e51a66a6aef18bb310521e4745fc526cb17
Jesse Glick <jesseglick@expertool.com>
2024-05-08 11:14:52 -0400

    Update crate overview.

--- src/lib.rs
+++ src/lib.rs
@@ -6,16 +6,21 @@
 //! is called the *full key*, the key by which aggregated results are grouped
 //! is called the *group key*, and that portion of the full key that does not
 //! form part of the *group key* is called the *aggregation key*.
+//! 
 //! The group key and aggregation key can each be simple or composite.
-//! The key values are assumed to be text.
 //!
 //! Two primary structures are provided: BilevelSet, which merely lists the
-//! aggregation keys associated with each group key, and BilevelMap,
-//! which allows a payload to be kept for each pairing.
+//! aggregation keys associated with each group key, and BilevelMap, which
+//! allows a payload to be kept for each pairing.
 //!
-//! For illustration purposes, the trivial case where group and aggregation key
-//! values are simple scalars rather than potentially composite text
-//! is also implemented.
+//! # Modules
+//! 
+//! - copy  Use the versions in this module where both the group key and the
+//!         aggregation key are copy types.
+//! - borrow Use the versions in this module where the group key is a copy
+//!         type but the aggregation key is not.
+//! - hybrid Use the versions in this module where neither key is a copy type.  
+
 
 use std::hash::{Hash, Hasher, DefaultHasher};
 
